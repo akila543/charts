3b4f727e5e840f4108102718a135020ce84211d4
AK354349 <akilandeswari.dhandapani@wipro.com>
2019-03-31 13:52:47 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const CHART = document.getElementById('lineChart');
+let lineChart = new Chart(CHART,{
+  type:'line',
+  data:{
+    labels: ["January","February","March","April","May","June","July","August"],
+    datasets:[{
+      label:"Bike Details",
+      fill:false,
+      lineTension:0.1,
+      backgroundColor:"rgba(75,192,192,0.4)",
+      borderColor:"rgba(75,192,192,1)",
+      borderCapStyle: 'butt',
+      borderDash:[],
+      borderDashOffset:0.0,
+      borderJoinStyle:'miter',
+      pointBorderColor:"rgba(75,192,192,1)",
+      pointBackgroundColor:"#fff",
+      pointBorderWidth:1,
+      pointHoverRadius:5,
+      pointHoverBackgroundColor:"rgba(75,192,192,1)",
+      pointHoverBorderColor:"rgba(220,220,220,1)",
+      pointRadius:1,
+      pointHitRadius:10,
+      data:[65,59,80,81,56,55,40]
+    }]
+  }
+})
